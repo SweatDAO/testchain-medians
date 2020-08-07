92e93383115bf3e37a7ed825d7b7aaecdf706f82
stefanionescu <stefanionescu@protonmail.com>
2020-08-07 16:10:34 +0100

    Update deps

--- src/ChainlinkMedianizers.sol
+++ src/ChainlinkMedianizers.sol
@@ -21,21 +21,21 @@ import "geb-chainlink-median/ChainlinkPriceFeedMedianizer.sol";
 
 // USD
 contract ChainlinkMedianRAIUSD is ChainlinkPriceFeedMedianizer {
-    constructor(address aggregator) ChainlinkPriceFeedMedianizer(aggregator) public {
+    constructor(address aggregator, address treasury, uint256 updateCallerReward) ChainlinkPriceFeedMedianizer(aggregator, treasury, updateCallerReward) public {
         symbol = "RAIUSD";
         multiplier = 10;
     }
 }
 
 contract ChainlinkMedianETHUSD is ChainlinkPriceFeedMedianizer {
-    constructor(address aggregator) ChainlinkPriceFeedMedianizer(aggregator) public {
+    constructor(address aggregator, address treasury, uint256 updateCallerReward) ChainlinkPriceFeedMedianizer(aggregator, treasury, updateCallerReward) public {
         symbol = "ETHUSD";
         multiplier = 10;
     }
 }
 
 contract ChainlinkMedianFLXUSD is ChainlinkPriceFeedMedianizer {
-    constructor(address aggregator) ChainlinkPriceFeedMedianizer(aggregator) public {
+    constructor(address aggregator, address treasury, uint256 updateCallerReward) ChainlinkPriceFeedMedianizer(aggregator, treasury, updateCallerReward) public {
         symbol = "FLXUSD";
         multiplier = 10;
     }
